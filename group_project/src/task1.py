cb902189d450fc674490baddc7add50b3baa5c2f
LeonHammerla <leon.hammerla@gmx.de>
2023-09-14 00:16:52 +0200

    .

--- group_project/src/task1.py
+++ group_project/src/task1.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 from typing import Optional, Dict, Callable, Any, Tuple
 from sklearn.manifold import TSNE, Isomap, SpectralEmbedding, MDS
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.manifold import LocallyLinearEmbedding as LLE
@@ -11,6 +10,8 @@ from sklearn.impute import SimpleImputer
 from sklearn import preprocessing
 from cebra import CEBRA
 import matplotlib.pyplot as plt
+import colorcet as cc
+from tqdm import tqdm
 
 BP = os.path.realpath(os.path.join(os.path.realpath(__file__), "../.."))
 
@@ -82,9 +83,11 @@ class Dataset:
                       time_point: int = 2,
                       dim: int = 2,
                       save: bool = True,
-                      train_svm: bool = False,
+                      do_train: bool = False,
+                      classifier: Optional[Callable[[np.ndarray, np.ndarray], Tuple[float, float]]] = None,
                       data_type: str = "bin",
-                      do_plot: bool = True
+                      do_plot: bool = True,
+                      show_plot: bool = True
                       ) -> Optional[float]:
         if data_type == "bin":
             # zB (8, 18, 8, 135, 160)
@@ -114,18 +117,18 @@ class Dataset:
         for i in range(orig_shape[1]):
             for j in [0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5]:
                 colors.append(j)
-        """# used
+        """  # used
         for i in range(orig_shape[1] // 2):
 
             # for j in [0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5]:
             for j in [0, 45, 90, 135, 0, 45, 90, 135]:
-            # for j in [0, 22.5, 45, 67.6, 0, 22.5, 45, 67.6]:
+                # for j in [0, 22.5, 45, 67.6, 0, 22.5, 45, 67.6]:
                 colors.append(j)
         for i in range(orig_shape[1] // 2, orig_shape[1]):
 
             # for j in [0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5]:
             for j in [22.5, 67.5, 112.5, 157.5, 22.5, 67.5, 112.5, 157.5]:
-            # for j in [90, 112.5, 135, 157.5, 90, 112.5, 135, 157.5]:
+                # for j in [90, 112.5, 135, 157.5, 90, 112.5, 135, 157.5]:
                 colors.append(j)
 
         colors = np.array(colors)
@@ -136,8 +139,8 @@ class Dataset:
             reduced_data = reduction_method(x, dim)
         reduced_data = preprocessing.normalize(reduced_data)
         f1 = None
-        if train_svm:
-            acc, f1 = self.train_svm(reduced_data, colors)
+        if do_train:
+            acc, f1 = classifier(reduced_data, colors)
         if do_plot:
             # plot for 2 dimensoins
             if dim == 2:
@@ -153,35 +156,44 @@ class Dataset:
                 plt.ylabel("Y")
                 plt.title(f"{reduction_method.__name__.split('_')[-1]}_{day}_dim={dim}_tp={time_point}")
                 plt.legend(*fig.legend_elements(),
-                           loc="lower right", title="Classes", bbox_to_anchor=(1.1, 0.5))
+                           title="Classes", bbox_to_anchor=(1.05, 1.0), loc='upper left')
+                plt.tight_layout()
                 if save:
                     os.makedirs(os.path.join(BP, "data", f"{day}__tp={time_point}"), exist_ok=True)
                     plt.savefig(os.path.join(BP, "data", f"{day}__tp={time_point}",
                                              f"{reduction_method.__name__.split('_')[-1]}_dim={dim}.png"))
-                plt.show()
+                if show_plot:
+                    plt.show()
+                else:
+                    plt.close("all")
             elif dim == 3:
                 fig = plt.figure()
                 ax = fig.add_subplot(projection='3d')
-                ax.scatter(*np.transpose(reduced_data),
-                           c=colors,
-                           s=100,
-                           cmap="hsv",
-                           vmin=0,
-                           vmax=180
-                           )
+                sc = ax.scatter(*np.transpose(reduced_data),
+                                c=colors,
+                                s=100,
+                                cmap="hsv",
+                                vmin=0,
+                                vmax=180
+                                )
                 plt.xlabel("X")
                 plt.ylabel("Y")
                 plt.ylabel("Z")
                 plt.title(f"{reduction_method.__name__.split('_')[-1]}_{day}_dim={dim}_tp={time_point}")
-                plt.legend()
+                plt.legend(*sc.legend_elements(), title="Classes", bbox_to_anchor=(1.05, 1.0), loc='upper left')
+                plt.tight_layout()
                 if save:
                     os.makedirs(os.path.join(BP, "data", f"{day}__tp={time_point}"), exist_ok=True)
                     plt.savefig(os.path.join(BP, "data", f"{day}__tp={time_point}",
                                              f"{reduction_method.__name__.split('_')[-1]}_dim={dim}.png"))
-                plt.show()
+                if show_plot:
+                    plt.show()
+                else:
+                    plt.close("all")
         return f1
 
-    def train_svm(self, x: np.ndarray, y: np.ndarray) -> Tuple[float, float]:
+    @staticmethod
+    def train_svm(x: np.ndarray, y: np.ndarray) -> Tuple[float, float]:
         from sklearn import svm
         from sklearn.metrics import classification_report
         from sklearn.utils import shuffle
@@ -193,10 +205,30 @@ class Dataset:
         y_train = y[:int(len(y) * 0.8)]
         y_test = y[int(len(y) * 0.8):]
         # train
-        clf = svm.SVC()
+        clf = svm.SVC(decision_function_shape='ovo')
         clf.fit(x_train, y_train)
         # test
         y_pred = clf.predict(x_test)
+        res = classification_report(y_test, y_pred, output_dict=True)
+        return res["accuracy"], res["weighted avg"]["f1-score"]
+
+    @staticmethod
+    def train_knn(x: np.ndarray, y: np.ndarray) -> Tuple[float, float]:
+        from sklearn.neighbors import KNeighborsClassifier
+        from sklearn.metrics import classification_report
+        from sklearn.utils import shuffle
+        le = preprocessing.LabelEncoder()
+        y = le.fit_transform(y)
+        x, y = shuffle(x, y, random_state=0)
+        x_train = x[:int(len(x) * 0.8)]
+        x_test = x[int(len(x) * 0.8):]
+        y_train = y[:int(len(y) * 0.8)]
+        y_test = y[int(len(y) * 0.8):]
+        # train
+        knn = KNeighborsClassifier(n_neighbors=7)
+        knn.fit(x_train, y_train)
+        # test
+        y_pred = knn.predict(x_test)
         res = classification_report(y_test, y_pred, output_dict=True)
         return res["accuracy"], res["weighted avg"]["f1-score"]
 
@@ -218,7 +250,6 @@ class Dataset:
         print()
         print(pca.explained_variance_)
         return pca_data
-
 
     @staticmethod
     def f_tsne(x: np.ndarray, dim: int, **kwargs) -> np.ndarray:
@@ -300,40 +331,60 @@ class Dataset:
         from matplotlib.animation import FuncAnimation
         nframes = 8
         plt.subplots_adjust(top=1, bottom=0, left=0, right=1)
-
+        os.makedirs(os.path.join(BP, "data", "gifs", day), exist_ok=True)
         def animate(i):
             im = plt.imread(os.path.join(BP, "data", f"{day}__tp={i}", f"{function_name}_dim={dim}.png"))
             plt.imshow(im)
 
         anim = FuncAnimation(plt.gcf(), animate, frames=nframes,
                              interval=(interval / nframes))
-        anim.save(filename=os.path.join(BP, "data/gifs", f"{day}_{function_name}_dim={dim}.gif"), writer='imagemagick')
+        anim.save(filename=os.path.join(BP, f"data/gifs/{day}", f"{day}_{function_name}_dim={dim}.gif"), writer='imagemagick')
 
     def run_all(self,
                 day: str = "Day_EO+6",
                 time_point: int = 2,
-                save: bool = True
+                save: bool = True,
+                dt: str = "bin"
                 ):
         for method in [i for i in dir(self) if i[:2] == "f_" and callable(getattr(self, i))]:
             method = getattr(self, method)
-            self.dim_reduction(method, day, time_point, 2, save)
+            self.dim_reduction(reduction_method=method, day=day, time_point=time_point, dim=2, save=save, data_type=dt, show_plot=False)
 
         for method in [i for i in dir(self) if i[:2] == "f_" and callable(getattr(self, i))]:
             method = getattr(self, method)
-            self.dim_reduction(method, day, time_point, 3, save)
+            self.dim_reduction(reduction_method=method, day=day, time_point=time_point, dim=3, save=save, data_type=dt, show_plot=False)
 
-    def run_all_whole_day(self, day: str):
+    def run_all_whole_day(self, day: str, dt: str = "bin"):
         for t in range(8):
-            ds.run_all(day=day, time_point=t)
+            ds.run_all(day=day, time_point=t, dt=dt)
         for method in [i for i in dir(self) if i[:2] == "f_" and callable(getattr(self, i))]:
             method = method.split("_")[-1]
             self.animate_plots(day=day, dim=2, function_name=method)
             self.animate_plots(day=day, dim=3, function_name=method)
 
-    def svm_day_fixed(self, day: str, dim=2):
-        plt.gca().set_prop_cycle(plt.cycler('color', plt.cm.jet(np.linspace(0, 1, num_plots))))
+    def run(self):
+        import warnings
+        warnings.filterwarnings('ignore')
+        x = ["-2", "+0", "+2", "+4", "+6"]
+        """bar = tqdm(total=3*len(x))
+        for dt in ["bin", "contra", "ipsi"]:
+            for i in x:
+                with HiddenPrints():
+                    self.run_all_whole_day(f"Day_EO{i}", dt=dt)
+                bar.update(1)"""
+        bar = tqdm(total=len(x))
+        for i in x:
+            with HiddenPrints():
+                self.run_all_whole_day(f"Day_EO{i}", dt="bin")
+            bar.update(1)
+
+    def train_day_fixed(self,
+                      classifier: Callable[[np.ndarray, np.ndarray], Tuple[float, float]],
+                      day: str, dim=2,
+                      ):
+        plot_colors = sns.color_palette(cc.glasbey_light, n_colors=20)
         x = [i for i in range(8)]
-        for method in [i for i in dir(self) if i[:2] == "f_" and callable(getattr(self, i))] + ["do_nothing"]:
+        for idx, method in enumerate([i for i in dir(self) if i[:2] == "f_" and callable(getattr(self, i))] + ["do_nothing"]):
             if method in ["f_rastermap"]:
                 pass
             else:
@@ -342,15 +393,46 @@ class Dataset:
                 with HiddenPrints():
                     for j in x:
                         if method.__name__ == "do_nothing":
-                            y.append(self.dim_reduction(method, day, time_point=j, dim=200, save=False, train_svm=True, do_plot=False))
+                            y.append(self.dim_reduction(method, day, time_point=j, dim=200, save=False, do_train=True,
+                                                        do_plot=False, classifier=classifier))
                         else:
-                            y.append(self.dim_reduction(method, day, time_point=j, dim=dim, save=False, train_svm=True, do_plot=False))
-                plt.plot(x, y, label=method.__name__.split("_")[-1])
+                            y.append(self.dim_reduction(method, day, time_point=j, dim=dim, save=False, do_train=True,
+                                                        do_plot=False, classifier=classifier))
+                plt.plot(x, y, label=method.__name__.split("_")[-1], color=plot_colors[idx % len(plot_colors)])
         plt.xlabel("timepoint")
         plt.ylabel("f1-Score")
         plt.title(f"Day:{day}__Dim:{dim}")
-        plt.legend(loc="upper left")
+        plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+        plt.tight_layout()
         plt.show()
+
+    def train_tp_fixed(self,
+                      classifier: Callable[[np.ndarray, np.ndarray], Tuple[float, float]],
+                      tp: int, dim=2):
+        plot_colors = sns.color_palette(cc.glasbey_light, n_colors=20)
+        x = ["-2", "+0", "+2", "+4", "+6"]
+        for idx, method in enumerate([i for i in dir(self) if i[:2] == "f_" and callable(getattr(self, i))] + ["do_nothing"]):
+            if method in ["f_rastermap"]:
+                pass
+            else:
+                y = []
+                method = getattr(self, method)
+                with HiddenPrints():
+                    for j in x:
+                        if method.__name__ == "do_nothing":
+                            y.append(self.dim_reduction(method, day=f"Day_EO{j}", time_point=tp, dim=200, save=False, do_train=True,
+                                                        do_plot=False, classifier=classifier))
+                        else:
+                            y.append(self.dim_reduction(method, day=f"Day_EO{j}", time_point=tp, dim=dim, save=False, do_train=True,
+                                                        do_plot=False, classifier=classifier))
+                plt.plot(x, y, label=method.__name__.split("_")[-1], color=plot_colors[idx % len(plot_colors)])
+        plt.xlabel("timepoint")
+        plt.ylabel("f1-Score")
+        plt.title(f"TP:{tp}__Dim:{dim}")
+        plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+        plt.tight_layout()
+        plt.show()
+
 
 
 if __name__ == "__main__":
@@ -368,7 +450,9 @@ if __name__ == "__main__":
     ds.task_1(Dataset.f_cebra, time_point=2)
     ds.task_1(Dataset.f_rastermap, time_point=2)
     """
-    ds.svm_day_fixed(day="Day_EO+6", dim=3)
+    ds.run()
+    # ds.train_day_fixed(ds.train_knn, day="Day_EO+4", dim=3)
+    # ds.train_tp_fixed(ds.train_knn, tp=6, dim=3)
     # ds.dim_reduction(ds.f_isomap, day="Day_EO-2", time_point=4, dim=3, train_svm=True)
     # ds.dim_reduction(Dataset.f_cebra, time_point=1, dim=3)
     # ds.dim_reduction(Dataset.f_pca, time_point=2, dim=3)
@@ -379,4 +463,3 @@ if __name__ == "__main__":
     f = "isomap"
     ds.animate_plots(day="Day_EO+6", dim=2, function_name=f)
     ds.animate_plots(day="Day_EO+6", dim=3, function_name=f)"""
-
